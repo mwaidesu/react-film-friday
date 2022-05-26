ceb2645e02afa77f749007d70244fd07c108b8d6
Mwai <machariamwai@gmail.com>
2022-05-27 01:41:48 +0300

    Add firebase auth

--- react-movie-app/src/App.js
+++ react-movie-app/src/App.js
@@ -7,24 +7,39 @@ import { MovieDetails } from "./components/movieDetails/MovieDetails";
 import { Routes, Route } from "react-router-dom";
 
 import { FavoriteMovies } from "./components/FavoriteMovies";
+import Signin from "./components/SignIn";
+import Signup from "./components/SignUp";
+import { AuthContextProvider } from "./components/context/AuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
   return (
-    <div>
-        <Routes>
-          <Route exact path="/" element={<Dashboard />}></Route>
+    <AuthContextProvider>
+      <Routes>
+        <Route exact path="/" element={<Signin />}></Route>
 
-          <Route path="action" element={<ActionMovies />}></Route>
+        <Route path="/signup" element={<Signup />}></Route>
 
-          <Route path="horror" element={<Horror />}></Route>
+        <Route
+          path="/action"
+          element={
+            <ProtectedRoute>
+              <ActionMovies />
+            </ProtectedRoute>
+          }
+        ></Route>
 
-          <Route path="favorites" element={<FavoriteMovies/>}></Route>
+        <Route path="/action" element={<ActionMovies />}></Route>
 
-          <Route exact path="/movie/:id" element={<MovieDetails />}></Route>
+        <Route path="/horror" element={<Horror />}></Route>
 
-          <Route path="*" element={<FourOhFour />}></Route>
-        </Routes>
-    </div>
+        <Route path="favorites" element={<FavoriteMovies />}></Route>
+
+        <Route exact path="/movie/:id" element={<MovieDetails />}></Route>
+
+        <Route path="*" element={<FourOhFour />}></Route>
+      </Routes>
+    </AuthContextProvider>
   );
 };
 
--- react-movie-app/src/components/Dashboard.js
+++ react-movie-app/src/components/Dashboard.js
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import "./../index.css";
 import Movie from "./Movie";
 import useLocalStorage from "use-local-storage";
-import {Link} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { UserAuth } from './context/AuthContext';
+
 
 const apiKey = "api_key=efb8d52d72f50732f48333d163521021";
 const mainURL = "https://api.themoviedb.org/3";
@@ -12,6 +14,9 @@ const searchURL = `${mainURL}/search/movie?${apiKey + "&query="}`;
 function Dashboard() {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+
+  const { user, logout } = UserAuth();
+  const navigate = useNavigate();
 
   const [theme, setTheme] = useLocalStorage("theme" ? "dark" : "light");
   const switchTheme = () => {
@@ -45,54 +50,68 @@ function Dashboard() {
     setSearchTerm(event.target.value);
   };
 
-  return (
-    <div data-theme={theme}>
-      <header className="header">
-        <form onSubmit={handleOnSubmit}>
-          <input
-            type="search"
-            placeholder="Film Fridays..."
-            className="search"
-            value={searchTerm}
-            onChange={handleOnChange}
-          />
-        </form>
-      </header>
 
-      <div className="theme-div">
-        <Link to="/horror">
-          <button className="theme-button bg-black px-3 py-2 text-white rounded-lg border-solid border-white border-2">
-            Horror Movies
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate('/');
+      console.log('You are logged out')
+    } catch (e) {
+      console.log(e.message);
+    }
+
+    return (
+      <div data-theme={theme}>
+        <header className="header">
+          <form onSubmit={handleOnSubmit}>
+            <input
+              type="search"
+              placeholder={<p>User Email: {user && user.email}</p>}
+              className="search"
+              value={searchTerm}
+              onChange={handleOnChange}
+            />
+          </form>
+        </header>
+
+        <div className="theme-div">
+          <button onClick={handleLogout} className="border px-6 py-2 my-4">
+            Logout
           </button>
-        </Link>
+          
+          <Link to="/horror">
+            <button className="theme-button bg-black px-3 py-2 text-white rounded-lg border-solid border-white border-2">
+              Horror Movies
+            </button>
+          </Link>
 
-        <Link to="/action">
-          <button className="theme-button bg-black px-3 py-2 text-white rounded-lg border-solid border-white border-2">
-            Action Movies
+          <Link to="/action">
+            <button className="theme-button bg-black px-3 py-2 text-white rounded-lg border-solid border-white border-2">
+              Action Movies
+            </button>
+          </Link>
+
+          <Link to="/favorites">
+            <button className="theme-button bg-black px-3 py-2 text-white rounded-lg border-solid border-white border-2">
+              Favorites
+            </button>
+          </Link>
+
+          <button
+            className="theme-button bg-black px-3 py-2 text-white rounded-lg border-solid border-white border-2"
+            id="btn"
+            onClick={switchTheme}
+          >
+            Change Theme
           </button>
-        </Link>
+        </div>
 
-        <Link to="/favorites">
-          <button className="theme-button bg-black px-3 py-2 text-white rounded-lg border-solid border-white border-2">
-            Favorites
-          </button>
-        </Link>
-
-        <button
-          className="theme-button bg-black px-3 py-2 text-white rounded-lg border-solid border-white border-2"
-          id="btn"
-          onClick={switchTheme}
-        >
-          Change Theme
-        </button>
+        <div className="movie-container">
+          {movies.length > 0 &&
+            movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+        </div>
       </div>
-
-      <div className="movie-container">
-        {movies.length > 0 &&
-          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
-      </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default Dashboard;
+  export default Dashboard;
